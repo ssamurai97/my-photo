6805fe004b48330ec008999ca44386f577c85b2f
kuchlong kuchlong <kuchlong@gmail.com>
2022-09-18 17:41:16 -0500

    edit gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
 
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' });
-
-export default {
+// import dotenv from 'dotenv';
+//
+// dotenv.config({ path: '.env' });
+require('dotenv').config({path: '.env'})
+exports.module = {
   siteMetadata: {
     title: `photos`,
     siteUrl: 'https://gatsby.photo',
